3ce48083b548adc104cf72265f550f9584dd9553
Sümeyye Acar <99368893+smyy1001@users.noreply.github.com>
2023-07-27 16:19:10 +0300

    Update Lab02_Q1_acar.java

--- lab2/Lab02_Q1_acar.java
+++ lab2/Lab02_Q1_acar.java
@@ -1,3 +1,11 @@
+/*
+ * Course: CS-101
+ * Semester: Spring 2022
+ * Assignment: Lab02 - Q1
+ * Author: Sumeyye Acar
+ * Id: 22103640
+*/
+
 import java.util.Scanner;
 
 public class Lab02_Q1_acar {
